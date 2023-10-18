baa29f9a663b730a36704eba25c0e0f1ae36b872
Hellicat <jhccop@naver.com>
2023-10-18 20:45:14 +0900

    food

---
+++ README.md
@@ -0,0 +1,29 @@
+# Clone - Canal Street Market
+
+clone Coding
+
+https://canalstreet/market/food
+
+Develope by Pug / SCSS / Vanilla JS
+
+Built by Gulp
+
+Use Live Server(Vscode Extention)
+
+#### Develope
+
+```
+npm run dev
+```
+
+#### Build
+
+```
+npm run build
+```
+
+#### Deploy
+
+```
+npm run deploy
+```
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,54 @@
+const globalNav = document.querySelectorAll(".header__nav > ul > li");
+
 const wrapCont = document.querySelectorAll(".wrapper > div");
 
-wrapCont.forEach((ele, key, parent) => {
-  function handleWrap() {
-    console.log(ele.children);
+globalNav.forEach((e, n, p) => {
+  function handleNavClick() {
+    if (n === 0) {
+      p[1].classList.remove("move-nav");
+      p[2].classList.remove("move-nav");
+      p[3].classList.remove("move-nav");
+      p[0].childNodes[0].classList.add("hidden");
+      p[1].childNodes[0].classList.remove("hidden");
+      p[2].childNodes[0].classList.remove("hidden");
+      p[3].childNodes[0].classList.remove("hidden");
+      wrapCont[1].classList.remove("body__active");
+      wrapCont[2].classList.remove("body__active");
+      wrapCont[3].classList.remove("body__active");
+    } else if (n === 1) {
+      p[1].classList.add("move-nav");
+      p[2].classList.remove("move-nav");
+      p[3].classList.remove("move-nav");
+      p[0].childNodes[0].classList.remove("hidden");
+      p[1].childNodes[0].classList.add("hidden");
+      p[2].childNodes[0].classList.remove("hidden");
+      p[3].childNodes[0].classList.remove("hidden");
+      wrapCont[1].classList.add("body__active");
+      wrapCont[2].classList.remove("body__active");
+      wrapCont[3].classList.remove("body__active");
+    } else if (n === 2) {
+      p[1].classList.add("move-nav");
+      p[2].classList.add("move-nav");
+      p[3].classList.remove("move-nav");
+      p[0].childNodes[0].classList.remove("hidden");
+      p[1].childNodes[0].classList.remove("hidden");
+      p[2].childNodes[0].classList.add("hidden");
+      p[3].childNodes[0].classList.remove("hidden");
+      wrapCont[1].classList.add("body__active");
+      wrapCont[2].classList.add("body__active");
+      wrapCont[3].classList.remove("body__active");
+    } else if (n === 3) {
+      p[1].classList.add("move-nav");
+      p[2].classList.add("move-nav");
+      p[3].classList.add("move-nav");
+      p[0].childNodes[0].classList.remove("hidden");
+      p[1].childNodes[0].classList.remove("hidden");
+      p[2].childNodes[0].classList.remove("hidden");
+      p[3].childNodes[0].classList.add("hidden");
+      wrapCont[1].classList.add("body__active");
+      wrapCont[2].classList.add("body__active");
+      wrapCont[3].classList.add("body__active");
+    }
   }
-
-  ele.addEventListener("click", handleWrap);
+  e.addEventListener("click", handleNavClick);
 });
-
-console.log(wrapCont);
